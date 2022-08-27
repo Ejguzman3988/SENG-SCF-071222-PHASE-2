923348c75ffe86f1907509cb7305fd3762e9c244
Eriebrto Guzman <eriberto.guzman@outlook.com>
2022-08-27 10:24:46 -0400

    BREAK GIFS

--- 04_react_forms/scf-dribbble/src/App.js
+++ 04_react_forms/scf-dribbble/src/App.js
@@ -2,25 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import ImageList from './components/ImageList';
 import coffee from "./CoffeeBreak.gif"
+import tea from "./Tea.gif"
+import donut from "./donut.gif"
+import couch from "./couch.webp"
 
 
 function App() {
   return (
     <div className="App">
-      <ImageList /> 
+      {/* <ImageList />  */}
       <header className="App-header">
+        <img src={couch} alt="logo" />
         <img src={coffee} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={tea} className="App-logo" alt="logo" />
+        <img src={donut} className="App-logo" alt="logo" />
       </header>
     </div>
   );
